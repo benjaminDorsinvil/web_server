ffedcb2b3c2cb5393b64e2986e12f4676edbb462
benjamindorsinvil <bdorsinvil@gmail.com>
2024-06-09 17:43:09 -0600

    adding new function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, Responder};
 
+//define out endpoint.
+#[actix_web::get("/greet")]
+async fn greet() -> impl Responder {
+    format!("Hello World");
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("Starting server on port {}", port);
 
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().service())
         .bind(("127.0.01", port))?
+        .workers(2)
         .run()
         .await
 }
