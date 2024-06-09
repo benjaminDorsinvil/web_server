317b586cb53b010f9fa42002b924664da5145e29
benjamindorsinvil <bdorsinvil@gmail.com>
2024-06-09 17:49:37 -0600

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{App, HttpServer, Responder};
 //define out endpoint.
 #[actix_web::get("/greet")]
 async fn greet() -> impl Responder {
-    format!("Hello World");
+    format!("Hello World!")
 }
 
 #[actix_web::main]
@@ -11,7 +11,7 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("Starting server on port {}", port);
 
-    HttpServer::new(|| App::new().service())
+    HttpServer::new(|| App::new().service(greet))
         .bind(("127.0.01", port))?
         .workers(2)
         .run()
