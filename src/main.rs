2c71c6a9aeb6955c3cdc0444d9380ff94ebbbfe7
benjamindorsinvil <bdorsinvil@gmail.com>
2024-06-10 16:11:20 -0600

    added age to the user struct

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use serde::{
 #[derive(Serialize, Deserialize)]
 struct User{
     name: String, 
+    age: u32,
 }
 
 type UserDb = Arc<Mutex<HashMap<u32, User>>>;
@@ -42,6 +43,7 @@ async fn get_user(
 struct CreateUserResponse {
     id: u32, 
     name: String,
+    age: u32
 }
 
 #[actix_web::post("/users")]
@@ -52,10 +54,12 @@ async fn create_user(
     let mut db = db.lock().unwrap();
     let new_id = db.keys().max().unwrap_or(&0) + 1;
     let name = user_data.name.clone();
+    let age = user_data.age.clone();
     db.insert(new_id, user_data.into_inner());
     HttpResponse::Created().json(CreateUserResponse{
         id: new_id,
         name,
+        age,
     })
 }
 
